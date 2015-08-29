d84ae9a16d70ed82a0bafc57262423426013c300
David G Chaves <davidgchaves@gmail.com>
2015-08-29 20:56:07 +0200

    Add custom Flux-like Dispatcher

---
+++ app/dispatcher.js
@@ -0,0 +1,19 @@
+var uuid      = require('node-uuid'),
+    listeners = {};
+
+module.exports = {
+  register: function (callback) {
+    var id = uuid.v4();
+
+    listeners[id] = callback;
+    return id;
+  },
+  dispatch: function (payload) {
+    console.info("Dispatching...", payload);
+
+    for (var id in listeners) {
+      var listener = listeners[id];
+      listener(payload);
+    };
+  }
+}
