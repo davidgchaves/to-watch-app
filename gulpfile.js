d99717048391f50174c2e0cf1ecb944ed0df981f
David G Chaves <davidgchaves@gmail.com>
2015-08-29 13:03:32 +0200

    Add `bundle` gulp task
    Bundles our app with browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp        = require('gulp'),
     LiveServer  = require('gulp-live-server'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    browserify  = require('browserify'),
+    reactify    = require('reactify'),
+    source      = require('vinyl-source-stream');
 
 gulp.task('live-server', function () {
   var server = new LiveServer('server/main.js');
   server.start();
 });
 
-gulp.task('serve', ['live-server'], function () {
+gulp.task('bundle', function () {
+  return browserify({
+    entries: 'app/main.jsx',
+    debug: true
+  })
+  .transform(reactify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task('serve', ['bundle', 'live-server'], function () {
   browserSync.init(null, {
     proxy: "http://localhost:3000",
     port: 9001
