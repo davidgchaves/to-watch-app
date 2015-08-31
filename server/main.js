430479e4232cc475a727200c340b2375a744b7b4
David G Chaves <davidgchaves@gmail.com>
2015-08-31 17:44:20 +0200

    Isomorphism 101 - Render homepage from server
      - Define `reactOutput` tag to be filled with pre-render data.
      - Render Flicks component from server into `reactOutput`.

--- server/main.js
+++ server/main.js
@@ -1,11 +1,19 @@
-var express = require('express'),
-    app     = express(),
-    parser  = require('body-parser'),
-    db      = require('./database');
+var express    = require('express'),
+    app        = express(),
+    parser     = require('body-parser'),
+    React      = require('react'),
+    FlickModel = require('./models/FlickModel');
+
+require('./database');
+require('babel/register');
 
 app
   .get('/', function (req, res) {
-    res.render('./../app/index.ejs', {});
+    var reactVirtualApp = React.createFactory(require('./../app/components/FlickList.jsx'));
+    FlickModel.find(function (error, dbDoc) {
+      var serverGeneratedFlicksComponent = React.renderToString(reactVirtualApp({ flicks: dbDoc }));
+      res.render('./../app/index.ejs', { reactOutput: serverGeneratedFlicksComponent });
+    });
   })
   .use(express.static(__dirname + './../build'))
   .listen(3000);
